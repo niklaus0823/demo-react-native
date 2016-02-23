467028b8b8f6455e16d02ebf6ae3f2b86acdeee7
fengjie <fengjie@shinezone.com>
2016-02-23 13:15:56 +0800

    update config

--- demo/config/BaseConfigs.js
+++ demo/config/BaseConfigs.js
@@ -1,4 +1,5 @@
 export default {
-  REQUEST_URL: 'http://172.17.70.118:8081'
+  //REQUEST_URL: 'http://172.17.70.118:8081'
+  REQUEST_URL: 'http://192.168.1.101:8081'
   //REQUEST_URL: 'http://192.168.100.103:8081'
 }
